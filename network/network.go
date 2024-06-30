8f02e7f6695c25979e53fead91ae6f6e5f1dc968
hfdxiaoyu <1306933520@qq.com>
2024-06-30 10:53:25 +0800

    新增常用get post请求

---
+++ network/network.go
@@ -0,0 +1,64 @@
+package network
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"time"
+)
+
+func Get(uri string, heads map[string]string) ([]byte, error) {
+	req, err := http.NewRequest("GET", uri, nil)
+	if err != nil {
+		return []byte{}, err
+	}
+	if len(heads) != 0 {
+		for s, s2 := range heads {
+			req.Header.Add(s, s2)
+		}
+	}
+
+	req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36")
+	req.Header.Add("Content-Type", "application/json")
+	client := http.Client{
+		Timeout: 30 * time.Second,
+	}
+
+	do, err := client.Do(req)
+	if err != nil {
+		return []byte{}, err
+	}
+	defer do.Body.Close()
+	body, err := io.ReadAll(do.Body)
+	if err != nil {
+		return []byte{}, err
+	}
+	return body, nil
+}
+
+func Post(url string, heads map[string]string, data []byte) ([]byte, error) {
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(data))
+	if err != nil {
+		return []byte{}, err
+	}
+	if len(heads) != 0 {
+		for s, s2 := range heads {
+			req.Header.Add(s, s2)
+		}
+	}
+	req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36")
+	req.Header.Add("Content-Type", "application/json")
+	client := http.Client{
+		Timeout: 30 * time.Second,
+	}
+	do, err := client.Do(req)
+	if err != nil {
+		return []byte{}, err
+	}
+	defer do.Body.Close()
+	body, err := io.ReadAll(do.Body)
+	if err != nil {
+		return []byte{}, err
+	}
+	return body, nil
+}
